7d5709ad1e4061a03edbcc321fdaf26a306d4a2b
Rui Zhi Liew <ruizhi@outlook.my>
2023-09-12 10:08:11 +0800

    chore: format files

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom'
-import App from './App';
+import App from './App'
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />
-  }
+    path: '/',
+    element: <App />,
+  },
 ])
 
-export default router;
+export default router
