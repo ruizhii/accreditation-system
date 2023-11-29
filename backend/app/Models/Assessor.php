a90999dfe8c17e0775687228da6cb045982eda62
SofeasS <136982077+SofeasS@users.noreply.github.com>
2023-11-30 00:16:06 +0800

    Add files via upload

---
+++ backend/app/Models/Assessor.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+
+class Assessor extends Model implements Authenticatable
+{
+    use HasFactory;
+
+    CONST REGISTERED_STATUS = 'registered';
+    CONST REJECTED_STATUS = 'rejected';
+    CONST APPROVED_STATUS = 'approved';
+
+    protected $fillable = [
+        'name',
+        'ic',
+        'institution_name',
+        'telephone_no',
+        'email',
+        'password',
+        'status',
+    ];
+
+     /**
+     * Get the name of the unique identifier for the user.
+     *
+     * @return string
+     */
+    public function getAuthIdentifierName()
+    {
+        return 'id'; // Change this if your primary key column is named differently
+    }
+
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Get the token value for the "remember me" session.
+     *
+     * @return string
+     */
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    /**
+     * Set the token value for the "remember me" session.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    /**
+     * Get the column name for the "remember me" token.
+     *
+     * @return string
+     */
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
+}
