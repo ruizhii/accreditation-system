0d611ed207704703c5c61fada959cef9de56897c
Rui Zhi Liew <ruizhi@outlook.my>
2024-01-04 12:09:01 +0800

    feat: ug and pg accreditation status bar charts

--- backend/app/Filament/Widgets/PostgraduateAccreditationSubmissionStatusChart.php
+++ backend/app/Filament/Widgets/PostgraduateAccreditationSubmissionStatusChart.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Accreditation;
+use App\Models\AssessorProgramme;
 use Carbon\Carbon;
 use Filament\Widgets\ChartWidget;
 
@@ -16,11 +17,27 @@ class PostgraduateAccreditationSubmissionStatusChart extends ChartWidget
                                     ->whereHas('academic_programme', function($q) {
                                         $q->where('graduate_level', 'postgraduate');
                                     })->count();
+                
+        $ongoingCount = Accreditation::where('status', 'Pending')
+                                       ->whereHas('academic_programme', function($q) {
+                                           $q->where('graduate_level', 'postgraduate');
+                                       })->count();
+
+        $reviewingCount = AssessorProgramme::where('is_completed', false)
+                                             ->whereHas('academicProgramme', function($q) {
+                                                $q->where('graduate_level', 'postgraduate');
+                                             })->count();
+
+        $completedCount = AssessorProgramme::where('is_completed', true)
+                                             ->whereHas('academicProgramme', function($q) {
+                                                $q->where('graduate_level', 'postgraduate');
+                                             })->count();
+
         return [
             'datasets' => [
             [
                 'label' => 'Status',
-                'data' => [ $duedCount, 30, 10, 20 ],
+                'data' => [ $duedCount, $ongoingCount, $reviewingCount, $completedCount ],
                 'backgroundColor' => '#36A2EB',
                 'borderColor' => '#9BD0F5',
             ],
