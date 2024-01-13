39edcc523ea0b145be0f7fce3f2f7ac054dc50ae
SofeasS <136982077+SofeasS@users.noreply.github.com>
2024-01-13 15:00:55 +0800

    Update nav.blade.php

--- backend/resources/views/assessors/layouts/nav.blade.php
+++ backend/resources/views/assessors/layouts/nav.blade.php
@@ -1,11 +1,17 @@
 <div class="header">
     <div class="d-flex justify-content-between px-5 py-3" style="background-color:white;">
         <div>
-            <img src="{{ asset('images/um-logo.png') }}" alt="Logo" class="logo">
-            <a href="{{ route('assessors.index') }}">
-                <button class="btn btn-secondary">Home</button>
-            </a>
-
+            <p>
+                <img src="{{ asset('images/um-logo.png') }}" alt="Logo" class="logo"
+                    style ="width: 200px; height: 60px">
+                <a href="{{ route('assessors.index') }}">
+                    <button class="btn btn-secondary">Home</button>
+                </a>
+                <a href ="mailto: qmec@um.edu.my">
+                    <button class="btn btn-secondary">Contact us: qmec@um.edu.my</button>
+                </a>
+            </p>
+            
             @if (Route::currentRouteNamed('assessors.programme.section.show'))
                 <a href="{{ route('assessors.programme.show', ['id' => $programme_id]) }}">
                     <button type="button" class="btn btn-secondary">Area</button>
@@ -20,13 +26,12 @@
             <form action="{{ route('assessors.logout') }}" method="POST" style="display: none;" id="logout-form">
                 @csrf
             </form>
-            <p style="font-size:20px;">
+            <p style="font-size:18px;">
                 <a href="{{ route('assessors.profile') }}">
                     <i class="fa fa-user my-3" aria-hidden="true"></i> <span style="color:black">{{ auth('assessors')->user()->name }}</span><br>
                 </a>
                 <i class='fas fa-sign-out-alt'></i> <a href="#"
-                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><span style="color:black">Logout</span></a>
-                    <p>Contact us: <a href = "mailto: qmec@um.edu.my">qmec@um.edu.my</a></p>
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><span style="color:black">Logout</span></a>                    
             </p>
         </div>
     </div>
