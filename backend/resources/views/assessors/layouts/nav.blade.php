85aca0b0e0db5daee41b7c5fe0fa46295eb72fdd
SofeasS <136982077+SofeasS@users.noreply.github.com>
2024-01-17 09:47:47 +0800

    Update nav.blade.php

--- backend/resources/views/assessors/layouts/nav.blade.php
+++ backend/resources/views/assessors/layouts/nav.blade.php
@@ -1,33 +1,34 @@
 <div class="header">
-    <div class="d-flex justify-content-between px-5 py-3" style="background-color:white;">
-        <div>
+    <div class="d-flex justify-content-between px-5 py-3" style="background-color:white; align-items: center08">
+        <div style ="align-items: center; height: 100">
             <p>
                 <img src="{{ asset('images/um-logo.png') }}" alt="Logo" class="logo"
-                    style ="width: 200px; height: 60px">
+                    style ="width: 200px; height: 60px;">
                 <a href="{{ route('assessors.index') }}">
                     <button class="btn btn-secondary">Home</button>
                 </a>
                 <a href ="mailto: qmec@um.edu.my">
                     <button class="btn btn-secondary">Contact us: qmec@um.edu.my</button>
                 </a>
-            </p>
             
-            @if (Route::currentRouteNamed('assessors.programme.section.show'))
-                <a href="{{ route('assessors.programme.show', ['id' => $programme_id]) }}">
-                    <button type="button" class="btn btn-secondary">Area</button>
-                </a>
-
-                <button type="button" onclick="printPage()" class="btn btn-secondary">Print Area
-                    {{ $assessorProgrammeSection->assessorProgrammeArea->area }}</button>
-            @endif
+                @if (Route::currentRouteNamed('assessors.programme.section.show'))
+                    <a href="{{ route('assessors.programme.show', ['id' => $programme_id]) }}">
+                        <button type="button" class="btn btn-secondary">Area</button>
+                    </a>
+                    <button type="button" onclick="printPage()" class="btn btn-secondary">Print Area
+                        {{ $assessorProgrammeSection->assessorProgrammeArea->area }}</button>
+                @endif
+            </p>
         </div>
-        <div></div>
+
         <div class="text-right">
             <form action="{{ route('assessors.logout') }}" method="POST" style="display: none;" id="logout-form">
                 @csrf
             </form>
-            <p style="font-size:18px;">
-                <b>QMEC Academic Accreditation portal for Assessors</b> <br>
+            <p style="font-size:18px">
+            <div class="text-right">
+                <h6>QMEC Academic Accreditation Portal for Assessors</h6>
+            </div>
                 <a href="{{ route('assessors.profile') }}">
                     <i class="fa fa-user my-3" aria-hidden="true"></i> <span style="color:black">{{ auth('assessors')->user()->name }}</span><br>
                 </a>
